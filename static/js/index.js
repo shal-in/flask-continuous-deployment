36db9ddba4dd6366beca05038d2d82d602cb8dc3
shal-in <shalin.a.ahasan@gmail.com>
2024-04-18 10:27:16 +0100

    Example files

---
+++ static/js/index.js
@@ -0,0 +1,10 @@
+console.log('index.js')
+
+// Create a new paragraph element
+var newParagraph = document.createElement('p');
+
+// Set the text content of the paragraph
+newParagraph.textContent = 'index.js linked';
+
+// Append the new paragraph to the body
+document.body.appendChild(newParagraph);
